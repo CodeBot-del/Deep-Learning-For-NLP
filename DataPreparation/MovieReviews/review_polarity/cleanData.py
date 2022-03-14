c390e94f066ce5ede2fc74ba3c894d2b85cdfcfa
gracie2 <stevenmanangu360@gmail.com>
2022-03-14 19:47:02 +0300

    started cleaning text data for movie reviews

---
+++ DataPreparation/MovieReviews/review_polarity/cleanData.py
@@ -0,0 +1,15 @@
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r' )
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+filename = 'txt_sentoken/neg/cv000_29416.txt'
+text = load_doc(filename)
+# split into tokens by white space
+tokens = text.split()
+print(tokens)
