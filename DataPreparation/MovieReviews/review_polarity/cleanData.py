5d3835425a63f46af1611f10fa80c0eb1ea15797
gracie2 <stevenmanangu360@gmail.com>
2022-03-14 20:07:56 +0300

    Cleaned text using nltk and regex

--- DataPreparation/MovieReviews/review_polarity/cleanData.py
+++ DataPreparation/MovieReviews/review_polarity/cleanData.py
@@ -1,3 +1,8 @@
+from nltk.corpus import stopwords
+import string
+import regex as re
+
+
 # load doc into memory
 def load_doc(filename):
     # open the file as read only
@@ -8,8 +13,34 @@ def load_doc(filename):
     file.close()
     return text
 
-filename = 'txt_sentoken/neg/cv000_29416.txt'
-text = load_doc(filename)
+# filename = 'txt_sentoken/neg/cv000_29416.txt'  
+# text = load_doc(filename)
 # split into tokens by white space
-tokens = text.split()
+# tokens = text.split()
+
+#create a function to clean data
+# turn a doc into clean tokens
+def clean_doc(doc):
+    # split into tokens by white space
+    tokens = doc.split()
+
+    # prepare regex for char filtering
+    re_punc = re.compile( '[%s]' % re.escape(string.punctuation))
+
+    # remove punctuation from each word
+    tokens = [re_punc.sub( '' , w) for w in tokens]
+
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    # filter out short tokens
+    tokens = [word for word in tokens if len(word) > 1]
+    return tokens
+
+#load the document 
+filename = 'txt_sentoken/pos/cv000_29590.txt'
+text = load_doc(filename)
+tokens = clean_doc(text)
 print(tokens)
