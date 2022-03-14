c471448e38dd4feac7ca2941ea0cbffe52ea7b14
gracie2 <stevenmanangu360@gmail.com>
2022-03-14 19:40:38 +0300

    Loaded text data using a function

--- DataPreparation/MovieReviews/review_polarity/review.py
+++ DataPreparation/MovieReviews/review_polarity/review.py
@@ -1,14 +1,5 @@
 from os import listdir
 
-# load one file
-
-filename = 'txt_sentoken/neg/cv000_29416.txt'
-# open the file as read only
-file = open(filename, 'r' )
-# read all text
-text = file.read()
-# close the file
-file.close()
 
 #function to load doc into memory
 def load_doc(filename):
@@ -21,15 +12,20 @@ def load_doc(filename):
     return text
 
 #load all documents from the negative directory
-directory = 'txt_sentoken/neg'
+
+def process_docs(directory):
 #walk through all files in the folder 
-for filename in listdir(directory):
-    #skip the files that do not have the right extension 
-    if not filename.endswith(".txt"):
-        next
+    for filename in listdir(directory):
+        #skip the files that do not have the right extension 
+        if not filename.endswith(".txt"):
+            next
+            
+        # create the full path of the file to open
+        path = directory + '/' + filename
+        # load document
+        doc = load_doc(path)
+        print( 'Loaded %s' % filename)
         
-    # create the full path of the file to open
-    path = directory + '/' + filename
-    # load document
-    doc = load_doc(path)
-    print( 'Loaded %s' % filename)
+#specify directory to load
+directory = 'txt_sentoken/neg'
+process_docs(directory)
